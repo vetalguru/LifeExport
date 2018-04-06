a1ec74634a509e85dbf72c50e466a65440d617e1
Vitalii Shkibtan <vitaliy.shkibtan@softheme.com>
2018-04-06 18:47:21 +0300

    [library] change logic to get the revision

--- tools/build_version.ps1
+++ tools/build_version.ps1
@@ -5,8 +5,8 @@ $build_version_file="build_version.h"
 $build_number_file="BUILD"
 
 
-# Get current build GIT revision
-$revision_command=(git log --pretty=format:%h -n 1)
+# Get current build GIT revision (Current commits number)
+$revision_command=(git rev-list --count HEAD)
 $revision_string='#define VERSION_REVISION            '+$revision_command
 
 # Save to version file
@@ -15,7 +15,7 @@ $revision_string | Out-File $build_version_file
 
 # Get current build number. If it doesn't exist create new with build number 1
 $build_number=1
-If (Test-Path $build_number_file)
+if (Test-Path $build_number_file)
 {
 	# Read old number from file
 	$old_build_number_str=Get-Content $build_number_file
@@ -28,7 +28,7 @@ If (Test-Path $build_number_file)
 	$build_number_str="{0:0000}" -f $build_number
 	$build_number_str | Out-File $build_number_file
 }
-Else
+else
 {
 	# If it doesn't exist create new with build number 1
 	$build_number_str="{0:0000}" -f $build_number
